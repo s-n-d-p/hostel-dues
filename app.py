05c78a2103286cc25dcc15816b17b928b9bc4faf
Sandeep <sandeepg.97@gmail.com>
2018-11-24 21:04:31 +0530

    fixed capitalisation

--- app.py
+++ app.py
@@ -14,12 +14,16 @@ def homepage():
     if request.method == 'GET':
         return render_template('homepage.html',result = result)
     elif request.method == 'POST':
-        roll_no = request.form['roll_no'].encode('ascii')
-        record = session.query(DuesRecord).filter_by(roll_no = roll_no).one()
-        result = "Name: " + record.name + '\n'
-        result += "Roll No: " + record.roll_no + '\n'
-        result += "Due: " + str(record.due) + '\n'
-        return render_template('homepage.html',result = result)        
+        roll_no = request.form['roll_no'].encode('ascii').upper()
+        try:
+            record = session.query(DuesRecord).filter_by(roll_no = roll_no).one()
+            result = "Name: " + record.name + '\n'
+            result += "Roll No: " + record.roll_no + '\n'
+            result += "Due: " + str(record.due) + '\n'
+            return render_template('homepage.html',result = result)        
+        except:
+            result = "Invalid roll number"
+            return render_template('homepage.html',result = result)        
     else:
         pass 
 
